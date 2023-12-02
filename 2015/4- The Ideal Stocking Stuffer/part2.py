aee88df4b42ca7c81e1ecc96abf26cb5eb7c2502
ZEN <97170564+II-ZEN-II@users.noreply.github.com>
2023-12-03 10:26:54 +1100

    Add files via upload

---
+++ 2015/4- The Ideal Stocking Stuffer/part2.py
@@ -0,0 +1,28 @@
+import hashlib
+
+
+def hash_md5_hex(string):
+    result = hashlib.md5(string.encode())
+    return result.hexdigest()
+
+
+def find_smallest_number(key):
+    number = 1
+    while True:
+        possible_string = key + str(number)
+        hashed_string = hash_md5_hex(possible_string)
+
+        valid = True
+        for i in range(6):
+            if hashed_string[i] != '0':
+                valid = False
+
+        if valid:
+            break
+
+        number += 1
+
+    return number
+
+
+print(find_smallest_number('iwrupvqb'))
