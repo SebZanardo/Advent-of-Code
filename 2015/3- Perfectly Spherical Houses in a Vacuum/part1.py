2f811a115af3aaebdff3e185c55b0cbd2741a107
ZEN <97170564+II-ZEN-II@users.noreply.github.com>
2023-12-02 12:40:52 +1100

    Add files via upload

---
+++ 2015/3- Perfectly Spherical Houses in a Vacuum/part1.py
@@ -0,0 +1,18 @@
+with open('input1.txt', 'r') as file:
+    directions = file.readline()
+
+delivered_houses = set()
+x = 0
+y = 0
+for direction in directions:
+    delivered_houses.add((x, y))
+    match(direction):
+        case '>':
+            x += 1
+        case '<':
+            x -= 1
+        case '^':
+            y -= 1
+        case 'v':
+            y += 1
+print(len(delivered_houses))
